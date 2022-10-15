e2e09b6a3e04a15f21cbccd57b25e9da0b4bfa50
Dafarlog <Dafarlog@gmail.com>
2022-10-15 18:13:02 +0300

    add Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
+import Loader from './Loader/Loader';
 import Searchbar from './Searchbar/Searchbar';
 
 export const App = () => {
   return (
     <>
       <Searchbar />
+      <Loader />
     </>
   );
 };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,16 +1,36 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
+const INITIAL_STATE = {
+  query: '',
+};
+
 export default class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+  handlerChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+    console.log(this.state);
+  };
+  handlerSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ ...INITIAL_STATE });
+  };
+  reset = () => {};
   render() {
     return (
-      <header className="searchbar">
+      <header className="searchbar" onSubmit={e => this.props.handlerSubmit(e)}>
         <form className="form">
           <button type="submit" className="button">
             <span className="button-label">Search</span>
           </button>
 
           <input
+            onInput={e => this.handlerChange(e)}
+            name="query"
             className="input"
             type="text"
             autoComplete="off"
