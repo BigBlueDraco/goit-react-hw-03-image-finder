ce50c488e6ecd9519dbf3258f79257912f62d0a5
Dafarlog <Dafarlog@gmail.com>
2022-10-15 16:24:38 +0300

    add api query

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { getPictures } from 'services/pixabayAPI';
+
+const data = getPictures('cat');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {console.log(data)}
     <App />
   </React.StrictMode>
 );
---
+++ src/services/pixabayAPI.js
@@ -0,0 +1,11 @@
+import axios, { Axios } from 'axios';
+
+export async function getPictures(query) {
+  const res = await axios.get('https://pixabay.com/api', {
+    params: {
+      key: '29897039-6335e8959bf94ffd3acb5a033',
+      q: query,
+    },
+  });
+  return res.data;
+}
