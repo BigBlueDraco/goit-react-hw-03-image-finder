40e5eca8911dd55bc594f8ad931afcdacb94ded4
Dafarlog <Dafarlog@gmail.com>
2022-10-17 21:33:25 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { getPictures } from 'services/pixabayAPI';
-
-const data = getPictures('cat');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
